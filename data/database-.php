0cd8ca25a87e4a4ad4e4bae1b8e800d35a414aad
BoWang <ralwayne@163.com>
2018-01-24 20:30:54 +0800

    网站配置

--- data/database-.php
+++ data/database-.php
@@ -2,9 +2,9 @@
 return array(
     'DB_TYPE'   => 'mysql',
     'DB_HOST'   => '127.0.0.1',
-    'DB_USER'   => 'a0813180826',
-    'DB_PWD'   => '99673033',
-    'DB_NAME'   => 'a0813180826',
+    'DB_USER'   => 'root',
+    'DB_PWD'   => 'wb128364',
+    'DB_NAME'   => 'xj',
     'DB_PREFIX'   => 'ecs_',
     'DB_PORT'   => '3306',
     'DB_CHARSET'   => 'utf8',
